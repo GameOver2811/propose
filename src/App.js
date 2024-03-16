88fc76ac6a51f24aa14d6aad62a787818b5ef81a
Krishna <kdwivedi343@gmail.com>
2024-03-16 14:18:58 +0530

    More fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import './App.css';
 import img from "./images/img01.jpg";
 import img3 from "./images/imd-03.jpg"
@@ -8,44 +8,54 @@ import last from "./images/last.jpg";
 import bhad from "./images/bhad.jpg";
 function App() {
   const [imgPath, setImgPath] = useState(img);
-  const[text,setText] = useState("Please maan ja na🥹🥹🥹");
+  const [text, setText] = useState("Please maan ja na🥹🥹🥹");
+  const [upText,setUpText] = useState("Kaise ho madam ji😛");
 
-  const yes = (e)=>{
+  const yes = (e) => {
     e.preventDefault();
     setImgPath(img3);
     setText("Batao frr kaha chalogi date per");
     const No = document.getElementById("no");
     No.disabled = true;
+    const buttons = document.getElementsByClassName("btn");
+    setUpText("Ammi Tumake Bhalo bhasi😘😘");
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].style.display = "none";
+    }
   }
 
-  const no = (e)=>{
+  const no = (e) => {
     e.preventDefault();
-    if(text === "Please maan ja na🥹🥹🥹") {
+    if (text === "Please maan ja na🥹🥹🥹") {
       setImgPath(img2);
       setText("Nhi manogi 🤨🤨🤨🤨");
-    }else if(text === "Nhi manogi 🤨🤨🤨🤨") {
+      setUpText("Oooo chammak challo man jao na🥺");
+    } else if (text === "Nhi manogi 🤨🤨🤨🤨") {
       setImgPath(img4);
       setText("Maan jao na 🥺");
+      setUpText("Mere jaise maasum per taras nhi ata kya 😐");
     }
-    else if(text === "Maan jao na 🥺") {
+    else if (text === "Maan jao na 🥺") {
       setImgPath(last);
-      setText("waise mujhe kala jaadu bhi ata h 🙂");
+      setUpText("waise mujhe kala jaadu bhi ata h 🙂");
+      setText("Kya mtlb tu UP se h Bengal se nhi💀");
     }
     else {
       setImgPath(bhad);
-      setText("Bhadd Mai Jau Chulbuli Chudail🤢");
+      setUpText("Bandariya kahi ki🥱")
+      setText("Bhadd Mai Jao Chulbuli Chudail🤢");
       const buttons = document.getElementsByClassName("btn");
 
       for (let i = 0; i < buttons.length; i++) {
-          buttons[i].style.display = "none";
+        buttons[i].style.display = "none";
       }
     }
   }
   return (
-    <div className="App">
-      <h1 className='heading'>Kaise ho madam ji😛</h1>
-      <div className='img'>
-        <img src={imgPath}  alt="Cat"/>
+    <div className="App flex">
+      <h1 className='heading'>{upText}</h1>
+      <div className='img flex'>
+        <img src={imgPath} alt="Cat" />
         <div className='A_cute_text'>{text}</div>
         <div className='buttons'>
           <button className="btn" onClick={yes}>Yes😛</button>

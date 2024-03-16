0bb6abba41009847f00f9275fea8ebc884370822
Krishna <kdwivedi343@gmail.com>
2024-03-16 12:01:57 +0530

    Some edits

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import img from "./images/img01.jpg";
 import img3 from "./images/imd-03.jpg"
 import img2 from "./images/img-02.webp";
+import img4 from "./images/img04.jpg";
 import last from "./images/last.jpg";
+import bhad from "./images/bhad.jpg";
 function App() {
   const [imgPath, setImgPath] = useState(img);
   const[text,setText] = useState("Please maan ja na🥹🥹🥹");
@@ -21,21 +23,33 @@ function App() {
     if(text === "Please maan ja na🥹🥹🥹") {
       setImgPath(img2);
       setText("Nhi manogi 🤨🤨🤨🤨");
+    }else if(text === "Nhi manogi 🤨🤨🤨🤨") {
+      setImgPath(img4);
+      setText("Maan jao na 🥺");
     }
-    else {
+    else if(text === "Maan jao na 🥺") {
       setImgPath(last);
       setText("waise mujhe kala jaadu bhi ata h 🙂");
+    }
+    else {
+      setImgPath(bhad);
+      setText("Bhadd Mai Jau Chulbuli Chudail🤢");
+      const buttons = document.getElementsByClassName("btn");
+
+      for (let i = 0; i < buttons.length; i++) {
+          buttons[i].style.display = "none";
+      }
     }
   }
   return (
     <div className="App">
-      <h1 className='heading'>Hello!</h1>
+      <h1 className='heading'>Kaise ho madam ji😛</h1>
       <div className='img'>
         <img src={imgPath}  alt="Cat"/>
         <div className='A_cute_text'>{text}</div>
         <div className='buttons'>
-          <button onClick={yes}>Yes😛</button>
-          <button id="no" onClick={no}>No🤨</button>
+          <button className="btn" onClick={yes}>Yes😛</button>
+          <button className="btn" id="no" onClick={no}>No🤨</button>
         </div>
       </div>
     </div>
